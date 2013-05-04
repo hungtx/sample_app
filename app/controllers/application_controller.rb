efaeb01fc581ef33b1cf1b8fee543b26e8fee4f5
hung <xuanhung.ta@gmail.com>
2013-05-05 00:21:26 +0300

    finish microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,17 @@ class ApplicationController < ActionController::Base
     sign_out
     super
   end
+  
+   def edit
+      @user = User.find(params[:id])
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(params[:user])
+        # Handle a successful update.
+      else
+        render 'edit'
+      end
+    end
 end
